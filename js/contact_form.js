2da52857f45188d3379bf8a13b1a82f6f02a03e0
buzzlightyear182 <janeheatherdee@gmail.com>
2016-06-17 14:54:05 +0800

    Added warning and success messages on send

---
+++ js/contact_form.js
@@ -0,0 +1,24 @@
+$(function() {
+  $("#contactForm").validate({
+  submitHandler: function(form) {
+    $.ajax({
+      url: "//formspree.io/janeheatherdee@gmail.com",
+      method: "POST",
+      data: {
+        name: $(form).find("input[name='name']").val(),
+        _replyto: $(form).find("input[name='_replyto']").val(),
+        phone: $(form).find("input[name='phone']").val(),
+        message: $(form).find("textarea[name='message']").val()
+      },
+      dataType: "json",
+      success: function() {
+        $("#submit-success").fadeIn();
+        $("#contact-form").fadeOut();
+      },
+      error: function() {
+        $("#submit-errors").fadeIn();
+      }
+    });
+  }
+});
+})
